3a87208d8aac36ca3a2d1d9f94317c7bc95e81fc
Eric Ong <18691081@student.curtin.edu.au>
2020-08-17 12:46:04 +0800

    Make the render page swappable (fixed)

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -13,38 +13,40 @@ export default class MainMenu extends Component {
     render() {
         return (
             <HashRouter>
-                <div className="container p-4 py-5" style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)', border: '1px solid #000' }}>
-                    <div className="row mb-5">
-                        <div className="col-sm-4">
-                            <h3>LOGO</h3>
-                        </div>
-                        <div className="col-sm-8 text-right">
-                            <h3>Settings <span><i className="fa fa-cogs" /></span></h3>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-6" style={{ minHeight: '500px' }}>
-                            <div className="h-100 w-100" style={{ border: '1px solid #000' }}>
-                                <div className="position-absolute text-center" style={{ left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}>
-                                    <h3 className="mb-4">SIGNAL GENERATION</h3>
-                                    <NavLink className="btn btn-primary bg-dark text-white border-0" to='/Generate'>GENERATE</NavLink>
-                                    <p className="mt-4">Description Here</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-sm-6" style={{ minHeight: '500px' }}>
-                            <div className="h-100 w-100" style={{ border: '1px solid #000' }}>
-                                <div className="position-absolute text-center" style={{ left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}>
-                                    <h3 className="mb-4">Title Here</h3>
-                                    <NavLink className="btn btn-primary bg-dark text-white border-0" to='/Analyzer'>ANALYSE</NavLink>
-                                    <p className="mt-4">Description Here</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div className="row">
                     <Switch>
+                        <Route exact path="/" render={() => (
+                            <div id="mainmenu" className="container p-4 py-5" style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)', border: '1px solid #000' }}>
+                                <div className="row mb-5">
+                                    <div className="col-sm-4">
+                                        <h3>LOGO</h3>
+                                    </div>
+                                    <div className="col-sm-8 text-right">
+                                        <h3>Settings <span><i className="fa fa-cogs" /></span></h3>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-sm-6" style={{ minHeight: '500px' }}>
+                                        <div className="h-100 w-100" style={{ border: '1px solid #000' }}>
+                                            <div className="position-absolute text-center" style={{ left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}>
+                                                <h3 className="mb-4">SIGNAL GENERATION</h3>
+                                                <NavLink className="btn btn-primary bg-dark text-white border-0" to='/Generate'>GENERATE</NavLink>
+                                                <p className="mt-4">Description Here</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-sm-6" style={{ minHeight: '500px' }}>
+                                        <div className="h-100 w-100" style={{ border: '1px solid #000' }}>
+                                            <div className="position-absolute text-center" style={{ left: '50%', top: '50%', transform: 'translate(-50%,-50%)' }}>
+                                                <h3 className="mb-4">Title Here</h3>
+                                                <NavLink className="btn btn-primary bg-dark text-white border-0" to='/Analyzer'>ANALYSE</NavLink>
+                                                <p className="mt-4">Description Here</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            )} />
                         <Route path="/Generate" component={Generate} />
                         <Route path="/Analyzer" component={Analyzer} />
                     </Switch>
