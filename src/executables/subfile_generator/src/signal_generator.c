b003b745a988a8d0600179dd08c63b7961bee0ad
CryosisOS <nathanvdvelde@gmail.com>
2020-09-11 03:00:47 +0800

    Reformat and refactor of sibfile code - 191927600 Nathan van der Velde 20200911

--- src/executables/subfile_generator/src/signal_generator.c
+++ src/executables/subfile_generator/src/signal_generator.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
+#include "libs/frannor.h"
 #include "signal_generator.h"
 
 #define PI 3.14159265
 struct signal_generator
 {
-    int signalType; 
+    int signalType;
     //for sine wave
     float frequency;
     float phase;
@@ -17,42 +18,53 @@ struct signal_generator
     float noise_magnitude;
 };
 
+float getSineSignalValue(float time){
+    return (float) sin(2.0 * PI * time /180.0);
+}
+
+float getImpulseSignalValue(float time, float amplitude, float duration){
+    //give condition for finite impulse response
+    //with brief input ex: [0,0,1,0,0,1,0,0]
+    if((fmod(time, duration)) == 0.0){//impulse every (duration)
+        return amplitude;
+    }
+    else{
+        return 0.0; //baseline
+    }
+}
+
+float getRandomGaussianSignalValue(){
+    return 0.0;
+}
+
 //get signal at real 'time'
-//get value x = signal(t) 
+//get value x = signal(t)
 float getSignal(struct signal_generator signalGen ,float time){
     float value = 0.0;
     float t = signalGen.frequency * time + signalGen.phase;
     printf("signal at %f s \n", time);
     //return a value depend on the type of signal
-    switch (signalGen.signalType) 
-    {
-    case 0: //sine Wave : https://en.wikipedia.org/wiki/Waveform
-        value = (float) sin(2.0 * PI * t /180.0); //half circle 
-        break;
-    case 1: //impulse signal: infinite impulse response
-        //give condition for finite impulse response
-        //with brief input ex: [0,0,1,0,0,1,0,0]
-        if((fmod(time,signalGen.duration)) == 0.0)//impulse every (duration)
-            value = signalGen.amplitude;
-        else
-            value = 0.0; //baseline
-        break;
-    case 2: //gaussian signal
-
-        break;
-    default:
-        value = 0.0;
-        break;
+    switch (signalGen.signalType){
+        case 0: //sine Wave : https://en.wikipedia.org/wiki/Waveform
+            value = getSineSignalValue(t); //half circle
+            break;
+        case 1: //impulse signal: infinite impulse response
+            value = getImpulseSignalValue(time, signalGen.amplitude, signalGen.duration);
+            break;
+        case 2: //gaussian signal
+            value = getRandomGaussianSignalValue();
+            break;
     }
 
     return (value * signalGen.amplitude);
 }
 
 //uniform random nubmer generator in range [0,1]
+//Depricated, used a proper Additive Random Gaussian Noise Generator.
 float ranu(long* iseed){
     //iseed = 7^5 * (iseed % (m/a)) - (m%a) * (iseed/ (m/a))
     //update seed
-    *iseed = 16807 * (*iseed % 127773) - 2836 * (*iseed / 127773); 
+    *iseed = 16807 * (*iseed % 127773) - 2836 * (*iseed / 127773);
     if(*iseed < 0){
         *iseed += 2147483647;
     }
@@ -63,21 +75,34 @@ float add_noise(float signal, float noise){
     return signal + noise;
 }
 
+float* generateBlockRow(){
+    for(int ii = 0; ii <= 40; ii++){
+        
+    }
+}
+
 //testing purpose
 int main(int argc, char const *argv[])
 {
     struct signal_generator generator;
-    generator.signalType = 0; //choose type 
+    generator.signalType = 0; //choose type
     generator.phase = 0;
-    generator.frequency = 20; 
+    generator.frequency = 20;
     generator.amplitude = 1;
     generator.duration = 10;
-    
+
 
     time_t start = time(NULL);
 
-    long iseed = 123456;
+    unsigned int iseed;
     float noise[100]; //example noise container
+
+    if (-1 == (iseed = (unsigned int) time((time_t *) NULL))){
+		fprintf(stderr, "time() failed to set seed");
+		exit(0);
+	}
+
+    srannor(iseed); //Seed the random number generator
 
     //generate signal
     for (int i = 0; i < 100; i++)
@@ -86,8 +111,8 @@ int main(int argc, char const *argv[])
         getchar();
         time_t end = time(NULL);
         printf("perfect signal %f ", getSignal(generator, difftime(end, start)));
-        //generate noise and add noise to signal 
-        noise[i] = ranu(&iseed);
+        //generate noise and add noise to signal
+        noise[i] = (double) frannor();//Get the next value
         float noise_and_signal = add_noise(getSignal(generator, difftime(end, start)), noise[i]);
         printf("signal after add noise %f ", noise_and_signal);
 
--- src/executables/subfile_generator/src/subfile_generator.c
+++ src/executables/subfile_generator/src/subfile_generator.c
@@ -6,27 +6,22 @@
 
 const int NUM_TILES = 256;
 
-void write_example()
-{
+void write_example(){
     float numbers_example[256];
     FILE *fptr;
 
-    fptr = fopen("/mnt/d/LinuxProject/capstone/2020-17-software-signal-generator/BackEnd/cpp/example_delay.csv", "w");
+    fptr = fopen("../example_delay.csv", "w");
 
-    if (fptr == NULL)
-    {
+    if (fptr == NULL){
         printf("Can't open the file!");
     }
 
-    for (size_t i = 0; i < 256; i++)
-    {
+    for (size_t i = 0; i < 256; i++){
         numbers_example[i] = i + 0.5;
-        if (i == 255)
-        {
+        if (i == 255){
             fprintf(fptr, "%f", numbers_example[i]); //write to file
         }
-        else
-        {
+        else{
             fprintf(fptr, "%f,", numbers_example[i]); //write to file
         }
     }
@@ -34,28 +29,23 @@ void write_example()
 }
 
 //load 256 csv delay file
-void load_delay_file(char *filename, float *delay)
-{
+void load_delay_file(char *filename, float *delay){
     int counter = 0;
     delay = (float *) malloc(NUM_TILES);
 
     FILE *fptr;
     int lineRes;
     fptr = fopen(filename, "r");
-    if (fptr == NULL)
-    {
+    if (fptr == NULL){
         printf("Can't open the file!");
     }
 
-    do
-    {
+    do{
         lineRes = fscanf(fptr, "%f[^;\n]", &delay[counter]);
-        if (lineRes == 0)
-        {
+        if (lineRes == 0){
             lineRes = fscanf(fptr, "%*c");
         }
-        else
-        {
+        else{
             printf("counter number %d: %f\n", counter, delay[counter]);
             counter = counter + 1;
         }
@@ -66,13 +56,11 @@ void load_delay_file(char *filename, float *delay)
 }
 
 //DISPATCH handler
-static int callback(IniDispatch *disp, void *v_other)
-{
+static int callback(IniDispatch *disp, void *v_other){
 #define IS_KEY(KEY) \
     (ini_string_match_ii(KEY, disp->data, disp->format))
 
-    if (disp->type != INI_KEY)
-    {
+    if (disp->type != INI_KEY){
         return 0;
     }
 
@@ -88,16 +76,13 @@ static int callback(IniDispatch *disp, void *v_other)
     return 0;
 }
 
-void load_confini()
-{
-    if (load_ini_path("example_configfile.csv", INI_DEFAULT_FORMAT, NULL, callback, NULL))
-    {
-        printf("error, something went wrongat load path");
+void load_confini(){
+    if (load_ini_path("example_configfile.csv", INI_DEFAULT_FORMAT, NULL, callback, NULL)){
+        fprintf(stderr, "");
     }
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
     load_confini();
     return 0;
 }
