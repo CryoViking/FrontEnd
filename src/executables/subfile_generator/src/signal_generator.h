46b4413d74ffa695f8bc13679b009367079ca7b8
Evans Nguyen <long.np68@gmail.com>
2020-09-11 06:19:03 +0800

    fix minor error in makefile -19247171-philongnguyen-06:18(15m)

--- src/executables/subfile_generator/src/signal_generator.h
+++ src/executables/subfile_generator/src/signal_generator.h
@@ -1,3 +1,9 @@
 struct signal_generator;
 
+float getSineSignalValue(float time);
+float getImpulseSignalValue(float time, float amplitude, float duration);
+float getRandomGaussianSignalValue();
+float* generateBlockRow();
+float add_noise(float signal, float noise);
+float ranu(long* iseed);
 float getSignal(struct signal_generator signalGen ,float time);
