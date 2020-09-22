af048d2c5b892d4732d859ecffb72e377a5d54f4
CryosisOS <nathanvdvelde@gmail.com>
2020-09-22 12:50:09 +0800

    Updated to check git username on the commit

---
+++ src/executables/fractal_delay_application/main.py
@@ -0,0 +1,12 @@
+"""
+Author: Nathan van der Velde
+Date Created: 2020-9-22
+GitHub: CryosisOS
+"""
+
+def __main__():
+    print("Hello World")
+    print("Testing that git name is changed")
+
+if __name__ == "__main__":
+    __main__()
