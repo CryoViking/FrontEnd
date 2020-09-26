958aa65328e226bf856fbc7e0e4981566d8abbea
CryosisOS <nathanvdvelde@gmail.com>
2020-09-26 16:27:57 +0800

    Figured out Fourier Transform for signal generation and shifting Nathan van der Velde 19127600 20200926

--- src/executables/fractal_delay_application/main.py
+++ src/executables/fractal_delay_application/main.py
@@ -5,30 +5,22 @@ GitHub: CryosisOS
 """
 
 import numpy as np
+from numpy.random import seed
+from numpy.random import randn
+import random
 import scipy.interpolate
+from scipy import signal
 import matplotlib.pyplot as plt
 
-def getSineArray():
-    xarr = np.arange(0, 10, 0.1)
-    yarr = np.sin(xarr)
-    return xarr, yarr
+seed(1)
+x = range(100)
+waveform = [random.gauss(0, 127/3) for i in x]
+plt.plot(waveform, 'ro')
+#plt.show()
 
-x_parabola, y_parabola = getSineArray()
-print(x_parabola)
-print(y_parabola)
-
-plt.figure()
-u = plt.plot(x_parabola,y_parabola,'ro') # plot the points)
-plt.show()
-t = np.linspace(start=0, stop=len(x_parabola), num=1000*len(x_parabola)) # parameter t to parametrize x and y
-pxLagrange = scipy.interpolate.lagrange(t, x_parabola) # X(T)
-pyLagrange = scipy.interpolate.lagrange(t, y_parabola) # Y(T)
-n = 50
-ts = np.linspace(t[0],t[-1],n)
-xLagrange = pxLagrange(ts) # lagrange x coordinates
-yLagrange = pyLagrange(ts) # lagrange y coordinates
-plt.plot(xLagrange, yLagrange,'b-',label = "Polynomial")
+f = signal.resample(waveform, 100000)
+newX = np.linspace(0,100,100000)
+plt.plot(x, waveform, color="blue")
+plt.plot(newX, f,color="red")
 plt.show()
 
-#for y in yLagrange:
-#    print(y)
