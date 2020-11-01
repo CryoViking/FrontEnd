d85a72db93556692e8c1994bd683ae343ee4d53f
Tega Esabunor <tegaesabunor@gmail.com>
2020-11-01 20:05:48 +0800

    added build scripts and delay_generator project to resources subdirectory

---
+++ resources/delay_generator/src/delay_generator.h
@@ -0,0 +1,16 @@
+/**
+ * Author: Nathan van der Velde
+ * Date Created: 2020-09-02
+ * GitHub: CryosisOS
+ * StudentID: 19127600
+ */
+#ifndef DELAY_GENERATOR_H
+#define DELAY_GENERATOR_H
+
+#include <stdbool.h>
+
+char* generateOutputFilename();
+bool generateRandomDelays(char*);
+bool generateModelledDelays(char*, char*, double, double);
+
+#endif
