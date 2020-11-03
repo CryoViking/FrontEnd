79a00d10276a90c5784d3c0b68d079144ec4474d
18691081_EricOng <69432276+18691081@users.noreply.github.com>
2020-11-04 03:55:15 +0800

    SnackBar update

--- public/common.js
+++ public/common.js
@@ -24,23 +24,34 @@
         element1.addClass("d-none");
         element2.addClass("d-none");
     });
+
+    $("#getDelayNofy").click(function () {
+        var x = document.getElementById("delayNofy");
+        x.className = "show";
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    });
+
+    $("#getSubNofy").click(function () {
+        var x = document.getElementById("subNofy");
+        x.className = "show";
+        setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    });
+
+    $("#merge").click(function () {
+        var element1 = $("#unmerge-img1");
+        var element2 = $("#unmerge-img2");
+        var element3 = $("#merge-img");
+        element1.addClass("d-none");
+        element2.addClass("d-none");
+        element3.removeClass("d-none");
+    });
+
+    $("#unmerge").click(function () {
+        var element1 = $("#unmerge-img1");
+        var element2 = $("#unmerge-img2");
+        var element3 = $("#merge-img");
+        element3.addClass("d-none");
+        element2.removeClass("d-none");
+        element1.removeClass("d-none");
+    });
 });
-
-
-function merge() {
-   var element1 = $("#unmerge-img1");
-   var element2 = $("#unmerge-img2");
-   var element3 = $("#merge-img");
-   element1.addClass("d-none");
-   element2.addClass("d-none");
-   element3.removeClass("d-none");
-}
-
-function unmerge() {
-   var element1 = $("#unmerge-img1");
-   var element2 = $("#unmerge-img2");
-   var element3 = $("#merge-img");
-   element3.addClass("d-none");
-   element2.removeClass("d-none");
-   element1.removeClass("d-none");
-}
